e71fe55c06bb20a7952cefbd7735cc86884e5636
Nirmal Patel <46021840+Nirmal5@users.noreply.github.com>
2021-03-14 21:58:49 -0500

    Using Layout in all pages and consolidating styles

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,20 +1,33 @@
 import { Layout } from '@components';
 import { getPostBySlug } from '@lib/firebase';
-//import { useRouter } from 'next/router';
+import { useRouter } from 'next/router';
 import styles from '@styles/post.module.scss';
 import { getFormattedDate } from '@lib/utils';
+import {router} from "next/client";
 
-const PostPage = ({ post }) => (
-    <Layout>
-        <div className={styles.PostPage}>
-            <img src={post.coverImage} alt={post.coverImageAlt} />
-            <h1>{post.title}</h1>
-            <span>Published {getFormattedDate(post.dateCreated)}</span>
-            <p dangerouslySetInnerHTML={{__html: post.content}}/>
-        </div>
-    </Layout>
-);
+const PostPage = ({ post }) => {
+    const router = useRouter();
 
+    if (!post) {
+        router.push('/404');
+        return;
+    }
+
+    if (!post) {
+        return null;
+    }
+
+    return (
+        <Layout>
+            <div className={styles.PostPage}>
+                <img src={post.coverImage} alt={post.coverImageAlt} />
+                <h1>{post.title}</h1>
+                <span>Published {getFormattedDate(post.dateCreated)}</span>
+                <p dangerouslySetInnerHTML={{__html: post.content}}/>
+            </div>
+        </Layout>
+    );
+};
 
 export async function getServerSideProps(context) {
     const post = await getPostBySlug(context.query.slug);
